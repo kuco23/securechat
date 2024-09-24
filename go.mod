e9f761d83dd4f49f5bf76e62502f895934ab9068
kuco23 <nseverkar@gmail.com>
2024-09-24 19:33:07 +0200

    basic no auth, wss enabled socket

---
+++ go.mod
@@ -0,0 +1,7 @@
+module kuco23/go-socket
+
+go 1.18
+
+require github.com/gorilla/websocket v1.5.3
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,66 @@
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
+func serveWss(certPubPath string, certKeyPath string) {
+	// Create TLS configuration (for development or production)
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+	// Create HTTPS server using TLS
+	server := &http.Server{
+		Addr:      ":8443", // WSS typically uses port 443 (or 8443 for development)
+		TLSConfig: tlsConfig,
+	}
+	// Start the server with TLS
+	log.Printf("Starting WSS server on %v", server.Addr)
+	err := server.ListenAndServeTLS(certPubPath, certKeyPath)
+	if err != nil {
+		log.Fatalf("Failed to start WSS server: %v", err)
+	}
+}
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+
+	certPathPub := os.Getenv("CERT_PUB_PATH")
+	certPathKey := os.Getenv("CERT_KEY_PATH")
+	serveWss(certPathPub, certPathKey)
+}
